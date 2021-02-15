cbbf9282d6ab7eeec50859ab57e9c4c765d5ead5
Leonel Soriano <leonelsoriano3@gmail.com>
2021-02-15 10:47:57 -0300

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php  
 error_reporting(E_ALL ^ E_DEPRECATED);
-	$conn = mysql_connect('localhost', 'root', '19Elmarxista1');
+	$conn = mysql_connect('localhost', '', '');
 	 if (!$conn)
     {
 	 die('Could not connect: ' . mysql_error());
